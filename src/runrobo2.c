c25cc603ca8257bf4fa32ca51c7402863898716c
Alistair John Strachan <alistair@devzero.co.uk>
2010-02-07 23:24:35 +0000

    Revert Exo's subroutine fix, it broke regular jumps.

--- src/runrobo2.c
+++ src/runrobo2.c
@@ -3293,14 +3293,12 @@ void run_robot(World *mzx_world, int id, int x, int y)
         char label_buffer[ROBOT_MAX_TR];
         int cur_prog_line = cur_robot->cur_prog_line;
         int next_cmd = 0;
-        int last_prog_line;
 
         robot_box_display(mzx_world, cmd_ptr - 1, label_buffer, id);
 
         // Move to end of all box mesg cmds.
         do
         {
-          last_prog_line = cur_prog_line;
           cur_prog_line += program[cur_prog_line] + 2;
           // At next line- check type
           if(!program[cur_prog_line])
@@ -3310,15 +3308,13 @@ void run_robot(World *mzx_world, int id, int x, int y)
         } while((next_cmd == 47) || ((next_cmd >= 103) &&
          (next_cmd <= 106)) || (next_cmd == 116) || (next_cmd == 117));
 
-        cur_robot->cur_prog_line = last_prog_line;
+        cur_robot->cur_prog_line = cur_prog_line;
 
         // Send label
         if(label_buffer[0])
         {
           gotoed = send_self_label_tr(mzx_world, label_buffer, id);
         }
-
-        cur_robot->cur_prog_line = cur_prog_line;
 
         goto breaker;
       }
