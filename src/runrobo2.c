a58423c3e9ba3351f4291f4bc37cab7df661fffd
Alistair John Strachan <alistair@devzero.co.uk>
2010-02-08 21:48:40 +0000

    Revert commits 470/471.

--- src/runrobo2.c
+++ src/runrobo2.c
@@ -3291,32 +3291,30 @@ void run_robot(World *mzx_world, int id, int x, int y)
       {
         // Box messages!
         char label_buffer[ROBOT_MAX_TR];
-        int next_prog_line, next_cmd;
+        int cur_prog_line = cur_robot->cur_prog_line;
+        int next_cmd = 0;
 
         robot_box_display(mzx_world, cmd_ptr - 1, label_buffer, id);
 
         // Move to end of all box mesg cmds.
-        while(1)
+        do
         {
-          // jump command length, command length bytes, command length
-          next_prog_line = cur_robot->cur_prog_line +
-                           1 + program[cur_robot->cur_prog_line] + 1;
-
+          cur_prog_line += program[cur_prog_line] + 2;
           // At next line- check type
-          if(!program[next_prog_line])
+          if(!program[cur_prog_line])
             goto end_prog;
 
-          next_cmd = program[next_prog_line + 1];
-          if(!((next_cmd == 47) || ((next_cmd >= 103) && (next_cmd <= 106))
-                                || ((next_cmd >= 116) && (next_cmd <= 117))))
-            break;
+          next_cmd = program[cur_prog_line + 1];
+        } while((next_cmd == 47) || ((next_cmd >= 103) &&
+         (next_cmd <= 106)) || (next_cmd == 116) || (next_cmd == 117));
 
-          cur_robot->cur_prog_line = next_prog_line;
-        }
+        cur_robot->cur_prog_line = cur_prog_line;
 
         // Send label
         if(label_buffer[0])
+        {
           gotoed = send_self_label_tr(mzx_world, label_buffer, id);
+        }
 
         goto breaker;
       }
