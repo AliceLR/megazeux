8400421e133d16358f42edfd24ebadb0f79dc209
Alistair John Strachan <alistair@devzero.co.uk>
2010-02-08 21:49:19 +0000

    Revert wrong fixes to robot commenting.

--- src/robo_ed.c
+++ src/robo_ed.c
@@ -3326,16 +3326,11 @@ void robot_editor(World *mzx_world, Robot *cur_robot)
     {
       if(current_rline->validity_status == invalid_comment)
       {
-        if(current_rline->line_text_length > 235)
-          current_rline->line_text_length = 235;
-
         object_code_position[0] = current_rline->line_text_length + 3;
         object_code_position[1] = 107;
         object_code_position[2] = current_rline->line_text_length + 1;
 
-        strncpy(object_code_position + 3, current_rline->line_text,
-         current_rline->line_text_length - 1);
-        object_code_position[3 + current_rline->line_text_length - 1] = 0;
+        strcpy(object_code_position + 3, current_rline->line_text);
         object_code_position[current_rline->line_text_length + 4] =
          current_rline->line_text_length + 3;
         object_code_position += current_rline->line_text_length + 5;
