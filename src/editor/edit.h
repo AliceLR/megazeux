63b7bb5570d0c6bddb64f8907a0e1ffda345ab9d
Alistair John Strachan <alistair@devzero.co.uk>
2010-02-09 22:42:08 +0000

    Pave the way for the collab merge.

--- src/editor/edit.h
+++ src/editor/edit.h
@@ -28,6 +28,53 @@ __M_BEGIN_DECLS
 
 #include "../world_struct.h"
 
+typedef struct
+{
+  Board *src_board;
+  int cursor_x, cursor_y;
+  int cursor_board_x, cursor_board_y;
+  int scroll_x, scroll_y;
+  mzx_thing current_id;
+  int current_color;
+  int current_param;
+  int board_width, board_height;
+  int draw_mode;
+  int current_menu;
+  int show_level;
+  int block_x, block_y;
+  int block_dest_x, block_dest_y;
+  int block_command;
+  Board *block_board;
+  int text_place;
+  int text_start_x;
+  int modified;
+  int debug_x;
+  int saved_overlay_mode;
+  int edit_screen_height;
+  int copy_repeat_width;
+  int copy_repeat_height;
+  char *level_id;
+  char *level_param;
+  char *level_color;
+  char *overlay;
+  char *overlay_color;
+  int overlay_edit;
+  char current_world[128];
+  char mzm_name_buffer[128];
+  char current_listening_dir[MAX_PATH];
+  char current_listening_mod[512];
+  int listening_flag;
+  int saved_scroll_x[16];
+  int saved_scroll_y[16];
+  int saved_cursor_x[16];
+  int saved_cursor_y[16];
+  int saved_board[16];
+  int saved_debug_x[16];
+  Robot copy_robot;
+  Scroll copy_scroll;
+  Sensor copy_sensor;
+} EditorState;
+
 void edit_world(World *mzx_world);
 int place_current_at_xy(World *mzx_world, mzx_thing id, int color,
  int param, int x, int y, Robot *copy_robot, Scroll *copy_scroll,
