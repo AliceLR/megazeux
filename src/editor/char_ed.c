616e25766462dcf2b4dba76a34b448957b585d86
AliceLR <petrifiedrowan@gmail.com>
2013-01-04 01:02:51 -0700

    Fixed char editor F4/F5 bug w/ chars 254/255

--- src/editor/char_ed.c
+++ src/editor/char_ed.c
@@ -1620,7 +1620,7 @@ int char_editor(struct world *mzx_world)
           for(i2 = 0; i2 < highlight_width; i2++,
            char_offset++)
           {
-            char_offset = char_offset % 254; // Wrap away from the protected set
+            char_offset = char_offset % 256; // Wrap away from the protected set
             ec_load_char_ascii(char_offset);
           }
         }
@@ -1645,7 +1645,7 @@ int char_editor(struct world *mzx_world)
           for(i2 = 0; i2 < highlight_width; i2++,
            char_offset++)
           {
-            char_offset = char_offset % 254; // Wrap away from the protected set
+            char_offset = char_offset % 256; // Wrap away from the protected set
             ec_load_char_mzx(char_offset);
           }
         }
