81a511231844f2387b90b6b8bc598bdfad430f0b
AliceLR <petrifiedrowan@gmail.com>
2018-01-07 01:57:04 -0700

    Fixed manifest.sh

--- arch/manifest.sh
+++ arch/manifest.sh
@@ -10,7 +10,8 @@ cd $1
 rm -f manifest.txt
 
 for file in `find -mindepth 1 -type f -not -name config.txt \
-                                      -not -name pad.config | sort`; do
+                                      -not -name pad.config \
+                                      -not -name '*.debug' | sort`; do
 	size=`du -b $file | cut -f1`
 	sha256sum=`sha256sum -b $file | cut -f1 -d\ `
 	echo $sha256sum $size `echo $file | sed 's,\.\/,,'` \
