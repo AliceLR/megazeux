acfb99bb7ff3fe40f21812ae96ad9762a6d72038
AliceLR <petrifiedrowan@gmail.com>
2018-01-31 14:36:15 -0700

    Better color coding in run.sh output

--- testworlds/run.sh
+++ testworlds/run.sh
@@ -12,5 +12,7 @@ echo -e "$(\
   cat log/failures \
   | sed --expression='s/\[PASS\]/\[\\e\[92m\\e\[1mFAIL\\e\[0m\]/g' \
   | sed --expression='s/\[FAIL\]/\[\\e\[91m\\e\[1mFAIL\\e\[0m\]/g' \
-  | sed --expression='s/failures: ([0-9]*[1-9]|[0-9]+0)/failures: \\e\[90m\\e\[1m$1\\e\[0m/g' \
+  | sed --expression='s/\[[?][?][?][?]]/\[\\e\[93m\\e\[1m????\\e\[0m\]/g' \
+  | sed --expression='s/passes: \([1-9][0-9]*\)/passes: \\e\[92m\\e\[1m\1\\e\[0m/g' \
+  | sed --expression='s/failures: \([1-9][0-9]*\)/failures: \\e\[91m\\e\[1m\1\\e\[0m/g' \
   )"
