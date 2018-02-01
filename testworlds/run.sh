9a9b08bb203b9dc7a2bfece23b492f5674d157a0
AliceLR <petrifiedrowan@gmail.com>
2018-01-31 18:48:27 -0700

    Fixed testing from parent directory, added comments and bell

--- testworlds/run.sh
+++ testworlds/run.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
+cd "$(dirname "$0")"
 mkdir -p log
+
+# Standalone mode will allow tests.mzx to terminate MZX and no_titlescreen mode
+# simplifies things. Use the software renderer because we don't care about video
+# output performance.
 
 ../mzxrun \
   video_output=software \
   standalone_mode=1 \
   no_titlescreen=1 \
   tests.mzx
+
+# Color code PASS/FAIL tags and important numbers.
 
 echo -e "$(\
   cat log/failures \
@@ -16,3 +23,7 @@ echo -e "$(\
   | sed --expression='s/passes: \([1-9][0-9]*\)/passes: \\e\[92m\\e\[1m\1\\e\[0m/g' \
   | sed --expression='s/failures: \([1-9][0-9]*\)/failures: \\e\[91m\\e\[1m\1\\e\[0m/g' \
   )"
+
+# The pizza's done!
+
+echo -ne "\007"
