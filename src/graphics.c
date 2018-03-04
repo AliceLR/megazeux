30d746547c8149f1bb998f8eb92888ae10d45616
AliceLR <petrifiedrowan@gmail.com>
2018-03-04 12:13:34 -0700

    Fix bug where SDL 1.2 platforms would fail to build

--- src/graphics.c
+++ src/graphics.c
@@ -199,11 +199,11 @@ Sint32 ec_load_set_var(char *name, Uint16 pos, int version)
 
   if(!fp)
     return -1;
-  
+
   size = ftell_and_rewind(fp) / CHAR_SIZE;
   if(size + pos >= 256 && maxChars > 256 && !layer_renderer_check(true))
     maxChars = 256;
-  
+
   if(size + pos > maxChars)
     size = maxChars - pos;
 
@@ -1314,7 +1314,7 @@ void blank_layers(void)
    sizeof(struct char_element) * SCREEN_W * SCREEN_H);
   memset(graphics.video_layers[UI_LAYER].data, 0xFF,
    sizeof(struct char_element) * SCREEN_W * SCREEN_H);
-  
+
   // Fix the layer modes
   if (graphics.video_layers[BOARD_LAYER].mode != graphics.screen_mode)
   {
@@ -1461,10 +1461,13 @@ bool init_video(struct config_info *conf, const char *caption)
 bool has_video_initialized(void)
 {
 #ifdef CONFIG_SDL
+#if SDL_VERSION_ATLEAST(2,0,0)
   // Dummy SDL driver should act as headless.
   const char *sdl_driver = SDL_GetCurrentVideoDriver();
   if(sdl_driver && !strcmp(sdl_driver, "dummy")) return false;
-#endif
+#endif /* SDL_VERSION_ATLEAST(2,0,0) */
+#endif /* CONFIG_SDL */
+
   return graphics_was_initialized;
 }
 
@@ -2336,7 +2339,7 @@ void dump_screen(void)
       graphics.flat_intensity_palette[i] = (palette[i].r << 16) | (palette[i].g << 8) | (palette[i].b << 0);
       #endif /* PLATFORM_BYTE_ORDER == PLATFORM_BIG_ENDIAN */
     }
-    
+
     for (layer = 0; layer < graphics.layer_count; layer++)
     {
       graphics.sorted_video_layers[layer] = &graphics.video_layers[layer];
@@ -2348,7 +2351,7 @@ void dump_screen(void)
     }
 
     memcpy(graphics.flat_intensity_palette, backup_palette, sizeof(Uint32) * FULL_PAL_SIZE);
-    
+
     //dump_screen_real(ss, palette, make_palette(palette), name);
     dump_screen_real_32bpp(ss, name);
     free(ss);
@@ -2363,7 +2366,7 @@ void dump_char(Uint16 char_idx, Uint8 color, int mode, Uint8 *buffer)
   if (mode == -1) mode = graphics.screen_mode;
   char_idx = char_idx % PROTECTED_CHARSET_POSITION;
   color = color % SMZX_PAL_SIZE;
-  
+
   if (mode == 0) {
     cols[0] = (color & 0xF0) >> 4;
     cols[1] = color & 0x0F;
