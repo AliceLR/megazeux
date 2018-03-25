19b255a3d5f0a1c304a8a1c2a2663d0c51c81802
Ian Burgmyer <spectere@gmail.com>
2018-03-25 14:20:07 -0400

    More polish!
        * Merged 2.91c changes.
        * VERSION no longer gets populated with a leading 'v'
        * VERSION_DATE is now automatically derived from the build date.

--- arch/xcode/update_version.sh
+++ arch/xcode/update_version.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
+
 VERSION=$(/usr/libexec/PlistBuddy -c 'Print :CFBundleShortVersionString' MegaZeux/Info.plist)
-VER_START=${VERSION:0:1}
-NUM_MATCH='^[0-9]*$'
-
-OUTPUT='#define VERSION'
-
-if ! [[ $VER_START =~ $NUM_MATCH ]] ; then
-	# If the version doesn't start with a number (i.e. 'git'), leave off the 'v').
-	OUTPUT="$OUTPUT \"$VERSION\""
-else
-	OUTPUT="$OUTPUT \"v$VERSION\""
-fi
-
-echo "$OUTPUT" > version.h
+echo "#define VERSION \"${VERSION}\"" > version.h
+echo "#define VERSION_DATE \" (`date -u +%Y%m%d`)\"" >> version.h
 
