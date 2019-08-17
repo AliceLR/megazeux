1db9ed17f110583c9a691049d23b09da04a4c4b7
Adrian Siekierka <kontakt@asie.pl>
2019-08-17 11:20:07 +0200

    Emscripten: minify mzxrun_web.js in release mode

--- arch/emscripten/web/rollup.config.js
+++ arch/emscripten/web/rollup.config.js
@@ -2,15 +2,20 @@ import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 
-export default {
-  input: './src/index.js',
-  plugins: [
+let plugins = [
     resolve(),
     babel({
       exclude: 'node_modules/**' // only transpile our source code
     })
-//    ,terser()
-  ],
+];
+
+if (process.env.MINIFY) {
+    plugins.push(terser());
+}
+
+export default {
+  input: './src/index.js',
+  plugins: plugins,
   output: {
     file: 'mzxrun_web.js',
     format: 'iife', // use browser globals
