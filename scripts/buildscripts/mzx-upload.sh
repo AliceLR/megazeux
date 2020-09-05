45e731263bf2ffcd081196edca6e89db175f2333
AliceLR <petrifiedrowan@gmail.com>
2020-09-05 17:09:34 -0600

    Add uploads scripts

---
+++ scripts/buildscripts/mzx-upload.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+export MZX_SCRIPTS_BASE=$(cd $(dirname $0); pwd)
+export MZX_SCRIPTS="$MZX_SCRIPTS_BASE/mzx-scripts"
+export MZX_WORKINGDIR="$MZX_SCRIPTS_BASE/mzx-workingdir"
+export MZX_TARGET="TARGET/"
+
+source $MZX_SCRIPTS/uploads.sh
+
+#
+# Add lines to configure upload targets here.
+# "upload_curl" will POST the uploads via curl to a remote HTTP script.
+# "upload-ssh" will transfer the uploads via scp and then extract them via ssh.
+#
+
+# upload_curl "http://Put the URL of your upload PHP script here/.php"
+# upload_ssh  "username@example.com"
