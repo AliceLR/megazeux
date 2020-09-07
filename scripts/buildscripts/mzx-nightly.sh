2478fbaa3a97b5ce735b640fb4df44c90a1d797d
AliceLR <petrifiedrowan@gmail.com>
2020-09-07 02:00:38 -0600

    Avoid creating absolute paths starting with double slashes

--- scripts/buildscripts/mzx-nightly.sh
+++ scripts/buildscripts/mzx-nightly.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 export MZX_SCRIPTS_BASE=$(cd $(dirname $0); pwd)
-export MZX_SCRIPTS="$MZX_SCRIPTS_BASE/mzx-scripts"
-export MZX_WORKINGDIR="$MZX_SCRIPTS_BASE/mzx-workingdir"
-export MZX_TARGET="$(pwd)/TARGET/"
+export MZX_SCRIPTS="${MZX_SCRIPTS_BASE%/}/mzx-scripts"
+export MZX_WORKINGDIR="${MZX_SCRIPTS_BASE%/}/mzx-workingdir"
+export MZX_TARGET_BASE=$(pwd)
+export MZX_TARGET="${MZX_TARGET_BASE%/}/TARGET/"
 
 source $MZX_SCRIPTS/version.sh
 source $MZX_SCRIPTS/build.sh
