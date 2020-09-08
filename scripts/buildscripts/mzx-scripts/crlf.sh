86ba0833f373fe742d8ab33fce473f724e9c9951
AliceLR <petrifiedrowan@gmail.com>
2020-09-08 01:20:57 -0600

    Add missing include

--- scripts/buildscripts/mzx-scripts/crlf.sh
+++ scripts/buildscripts/mzx-scripts/crlf.sh
@@ -16,6 +16,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
+source "$MZX_SCRIPTS/common.sh"
+
 CARRIAGE_RETURN=$(printf '\r')
 
 #
