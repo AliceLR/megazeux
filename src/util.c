AliceLR/megazeux

commit a6bc67a96132f54610313201df4b518c0067e6b5

Use vfile et al. in main.c, configure.c, and file manager. (#290)

src/util.c
@@ -35,13 +35,14 @@
 #include <string.h>
 
 #ifdef __WIN32__
-#include "io/vfile_win32.h" // for windows.h, WIDE_PATHS, and utf16_to_utf8
+#include "io/vio_win32.h" // for windows.h, WIDE_PATHS, and utf16_to_utf8
 #endif
 
 #include "const.h" // for MAX_PATH
 #include "error.h"
 #include "platform.h"
 #include "io/path.h"
+#include "io/vio.h"
 
 struct mzx_resource
 {
@@ -186,8 +187,8 @@ static ssize_t find_executable_dir(char *dest, size_t dest_len, const char *argv
     ssize_t len = path_get_directory(dest, dest_len, argv0);
     if(len > 0)
     {
-      // chdir to it and get it as an absolute path.
-      if(!chdir(dest) && getcwd(dest, dest_len))
+      // Change to the executable dir and get it as an absolute path.
+      if(!vchdir(dest) && vgetcwd(dest, dest_len))
         return strlen(dest);
     }
 
@@ -246,11 +247,11 @@ int mzx_res_init(const char *argv0, boolean editor)
     }
 
     if(i == CONFIG_TXT)
-      chdir(CONFDIR);
+      vchdir(CONFDIR);
     else
-      chdir(SHAREDIR);
+      vchdir(SHAREDIR);
 
-    getcwd(p_dir, MAX_PATH);
+    vgetcwd(p_dir, MAX_PATH);
     p_dir_len = strlen(p_dir);
 
     full_path_len = p_dir_len + base_name_len + 2;
@@ -259,7 +260,7 @@ int mzx_res_init(const char *argv0, boolean editor)
     // The provided buffer should always be big enough, but check anyway.
     if(path_join(full_path, full_path_len, p_dir, mzx_res[i].base_name) > 0)
     {
-      if(!stat(full_path, &file_info))
+      if(!vstat(full_path, &file_info))
       {
         mzx_res[i].path = full_path;
         continue;
@@ -271,8 +272,8 @@ int mzx_res_init(const char *argv0, boolean editor)
     // Try to locate the resource relative to the bin_path
     if(bin_path)
     {
-      chdir(bin_path);
-      if(!stat(mzx_res[i].base_name, &file_info))
+      vchdir(bin_path);
+      if(!vstat(mzx_res[i].base_name, &file_info))
       {
         full_path_len = bin_path_len + base_name_len + 2;
         full_path = cmalloc(full_path_len);
@@ -334,41 +335,41 @@ const char *mzx_res_get_by_id(enum resource_id id)
   static char userconfpath[MAX_PATH];
   if(id == CONFIG_TXT)
   {
-    FILE *fp;
+    vfile *vf;
 
     // Special handling for CONFIG_TXT to allow for user
     // configuration files
     snprintf(userconfpath, MAX_PATH, "%s/%s", getenv("HOME"), USERCONFFILE);
 
     // Check if the file can be opened for reading
-    fp = fopen_unsafe(userconfpath, "rb");
+    vf = vfopen_unsafe(userconfpath, "rb");
 
-    if(fp)
+    if(vf)
     {
-      fclose(fp);
+      vfclose(vf);
       return (char *)userconfpath;
     }
     // Otherwise, try to open the file for writing
-    fp = fopen_unsafe(userconfpath, "wb");
-    if(fp)
+    vf = vfopen_unsafe(userconfpath, "wb");
+    if(vf)
     {
-      FILE *original = fopen_unsafe(mzx_res[id].path, "rb");
+      vfile *original = vfopen_unsafe(mzx_res[id].path, "rb");
       if(original)
       {
         size_t bytes_read;
         for(;;)
         {
-          bytes_read = fread(copy_buffer, 1, COPY_BUFFER_SIZE, original);
+          bytes_read = vfread(copy_buffer, 1, COPY_BUFFER_SIZE, original);
           if(bytes_read)
-            fwrite(copy_buffer, 1, bytes_read, fp);
+            vfwrite(copy_buffer, 1, bytes_read, fp);
           else
             break;
         }
-        fclose(fp);
-        fclose(original);
+        vfclose(vf);
+        vfclose(original);
         return (char *)userconfpath;
       }
-      fclose(fp);
+      vfclose(vf);
     }
 
     // If that's no good, just read the normal config file
@@ -380,7 +381,7 @@ const char *mzx_res_get_by_id(enum resource_id id)
 /**
  * Some platforms may not be able to display console output without extra work.
  * On these platforms redirect STDIO to files so the console output is easier
- * to read.
+ * to read. NOTE: this needs to use stdio, don't use vfile here.
  */
 boolean redirect_stdio(const char *base_path, boolean require_conf)
 {

arch/msvc/Core.vcxproj
@@ -310,7 +310,7 @@ echo #define VERSION_DATE " ($(BuildDate))"&gt;&gt;$(SolutionDir)version.h</Comm
     <ClCompile Include="..\..\src\io\dir.c" />
     <ClCompile Include="..\..\src\io\fsafeopen.c" />
     <ClCompile Include="..\..\src\io\path.c" />
-    <ClCompile Include="..\..\src\io\vfile.c" />
+    <ClCompile Include="..\..\src\io\vio.c" />
     <ClCompile Include="..\..\src\io\zip.c" />
     <ClCompile Include="..\..\src\io\zip_stream.c" />
     <ClCompile Include="..\..\src\legacy_board.c" />
@@ -414,7 +414,8 @@ echo #define VERSION_DATE " ($(BuildDate))"&gt;&gt;$(SolutionDir)version.h</Comm
     <ClInclude Include="..\..\src\io\memfile.h" />
     <ClInclude Include="..\..\src\io\path.h" />
     <ClInclude Include="..\..\src\io\vfile.h" />
-    <ClInclude Include="..\..\src\io\vfile_win32.h" />
+    <ClInclude Include="..\..\src\io\vio.h" />
+    <ClInclude Include="..\..\src\io\vio_win32.h" />
     <ClInclude Include="..\..\src\io\zip.h" />
     <ClInclude Include="..\..\src\io\zip_deflate.h" />
     <ClInclude Include="..\..\src\io\zip_deflate64.h" />

arch/msvc/Core.vcxproj.filters
@@ -405,7 +405,7 @@
     <ClCompile Include="..\..\src\io\path.c">
       <Filter>Source Files\io</Filter>
     </ClCompile>
-    <ClCompile Include="..\..\src\io\vfile.c">
+    <ClCompile Include="..\..\src\io\vio.c">
       <Filter>Source Files\io</Filter>
     </ClCompile>
     <ClCompile Include="..\..\src\io\zip.c">
@@ -725,7 +725,10 @@
     <ClInclude Include="..\..\src\io\vfile.h">
       <Filter>Header Files\io</Filter>
     </ClInclude>
-    <ClInclude Include="..\..\src\io\vfile_win32.h">
+    <ClInclude Include="..\..\src\io\vio.h">
+      <Filter>Header Files\io</Filter>
+    </ClInclude>
+    <ClInclude Include="..\..\src\io\vio_win32.h">
       <Filter>Header Files\io</Filter>
     </ClInclude>
     <ClInclude Include="..\..\src\io\zip.h">

src/Makefile.in
@@ -127,7 +127,7 @@ core_cobjs := \
   ${io_obj}/dir.o                 \
   ${io_obj}/fsafeopen.o           \
   ${io_obj}/path.o                \
-  ${io_obj}/vfile.o               \
+  ${io_obj}/vio.o                 \
   ${io_obj}/zip.o                 \
   ${io_obj}/zip_stream.o
 

src/audio/audio_mikmod.c
@@ -27,7 +27,7 @@
 
 #include "../const.h"
 #include "../util.h"
-#include "../io/vfile.h"
+#include "../io/vio.h"
 
 #ifdef _WIN32
 #define MIKMOD_STATIC

src/configure.c
@@ -35,6 +35,7 @@
 #include "util.h"
 #include "io/fsafeopen.h"
 #include "io/path.h"
+#include "io/vio.h"
 
 #define MAX_INCLUDE_DEPTH 16
 #define MAX_CONFIG_REGISTERED 2
@@ -633,7 +634,7 @@ static void config_startup_file(struct config_info *conf, char *name,
 
     // Make sure the startup path actually exists.
     if(conf->startup_path[0] &&
-     (stat(conf->startup_path, &stat_info) < 0 || !S_ISDIR(stat_info.st_mode)))
+     (vstat(conf->startup_path, &stat_info) < 0 || !S_ISDIR(stat_info.st_mode)))
       conf->startup_path[0] = '\0';
   }
   else
@@ -644,7 +645,7 @@ static void config_startup_path(struct config_info *conf, char *name,
  char *value, char *extended_data)
 {
   struct stat stat_info;
-  if(stat(value, &stat_info) || !S_ISDIR(stat_info.st_mode))
+  if(vstat(value, &stat_info) || !S_ISDIR(stat_info.st_mode))
     return;
 
   snprintf(conf->startup_path, 256, "%s", value);
@@ -1196,19 +1197,19 @@ void set_config_from_file(enum config_type type, const char *conf_file_name)
   char *extended_buffer;
   char *equals_position, *value;
   char *output_end_position = line_buffer + LINE_BUFFER_SIZE;
-  FILE *conf_file;
+  vfile *conf_file;
   int i;
 
   if(type >= NUM_CONFIG_TYPES)
     return;
 
-  conf_file = fopen_unsafe(conf_file_name, "rb");
+  conf_file = vfopen_unsafe(conf_file_name, "rb");
   if(!conf_file)
     return;
 
   extended_buffer = (char *)cmalloc(extended_allocate_size);
 
-  while(fsafegets(line_buffer_alternate, LINE_BUFFER_SIZE, conf_file))
+  while(vfsafegets(line_buffer_alternate, LINE_BUFFER_SIZE, conf_file))
   {
     if(line_buffer_alternate[0] != '#')
     {
@@ -1256,7 +1257,7 @@ void set_config_from_file(enum config_type type, const char *conf_file_name)
       if(line_buffer[0])
       {
         // There might be extended information too - get it.
-        peek_char = fgetc(conf_file);
+        peek_char = vfgetc(conf_file);
         extended_size = 1;
         extended_buffer_offset = 0;
         use_extended_buffer = NULL;
@@ -1266,7 +1267,7 @@ void set_config_from_file(enum config_type type, const char *conf_file_name)
         {
           // Extended data line
           use_extended_buffer = extended_buffer;
-          if(fsafegets(line_buffer_alternate, 254, conf_file))
+          if(vfsafegets(line_buffer_alternate, 254, conf_file))
           {
             // Skip any extra whitespace at the start of the line...
             char *line_buffer_pos = line_buffer_alternate;
@@ -1290,9 +1291,9 @@ void set_config_from_file(enum config_type type, const char *conf_file_name)
             extended_buffer_offset += line_size;
           }
 
-          peek_char = fgetc(conf_file);
+          peek_char = vfgetc(conf_file);
         }
-        ungetc(peek_char, conf_file);
+        vungetc(peek_char, conf_file);
 
         for(i = 0; i < config_registry[type].num_registered; i++)
         {
@@ -1306,7 +1307,7 @@ void set_config_from_file(enum config_type type, const char *conf_file_name)
   }
 
   free(extended_buffer);
-  fclose(conf_file);
+  vfclose(conf_file);
 }
 
 void set_config_from_command_line(int *argc, char *argv[])

src/editor/ansi.c
@@ -28,7 +28,7 @@
 #include "../robot.h"
 #include "../util.h"
 #include "../world_struct.h"
-#include "../io/vfile.h"
+#include "../io/vio.h"
 
 #include <stdio.h>
 #include <time.h>

src/editor/board.c
@@ -27,6 +27,7 @@
 #include "../world.h"
 #include "../world_format.h"
 #include "../util.h"
+#include "../io/vio.h"
 #include "../io/zip.h"
 
 #include "world.h"

commit 88d5bd6ef46cd8c65b705d357df402e01e11bbf6

Fix Linux breakage from a6bc67a9.

src/util.c
@@ -361,7 +361,7 @@ const char *mzx_res_get_by_id(enum resource_id id)
         {
           bytes_read = vfread(copy_buffer, 1, COPY_BUFFER_SIZE, original);
           if(bytes_read)
-            vfwrite(copy_buffer, 1, bytes_read, fp);
+            vfwrite(copy_buffer, 1, bytes_read, vf);
           else
             break;
         }