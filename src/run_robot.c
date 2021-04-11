AliceLR/megazeux

commit 57e798e6f0dbd6bafb7a5a49a7decff6cb7c2f7b

Allocate input_string separate from the board struct. (#291)
* Allocate board input_string separate from the board struct.
* Fix another debug menu RAM counting bug.
* Save charset_path, palette_path, and input_string when they are NULL.
* Changelog, add more tests, fix CLIP INPUT/IF FIRST STRING bounding.
* Add test for SET $string INPUT.
* Add 2.80 INPUT STRING expressions and misc test.

src/run_robot.c
@@ -3882,35 +3882,40 @@ void run_robot(context *ctx, int id, int x, int y)
       case ROBOTIC_CMD_INPUT: // input string
       {
         char input_buffer[ROBOT_MAX_TR];
+        char title_buffer[ROBOT_MAX_TR];
         char *break_pos;
+        size_t len;
 
-        tr_msg(mzx_world, cmd_ptr + 2, id, input_buffer);
-        input_buffer[71] = '\0';
+        tr_msg(mzx_world, cmd_ptr + 2, id, title_buffer);
+        title_buffer[71] = '\0';
 
         // Newlines break the UI. :(
-        break_pos = strchr(input_buffer, '\n');
+        break_pos = strchr(title_buffer, '\n');
         if(break_pos)
           *break_pos = '\0';
 
-        src_board->input_string[0] = 0;
+        input_buffer[0] = 0;
 
         dialog_fadein();
-        input_window(mzx_world, input_buffer, src_board->input_string, 70);
+        input_window(mzx_world, input_buffer, input_buffer, 70);
 
         // Due to a faulty check, 2.83 through 2.91f always stay faded in here.
         // If something is found that relies on that, make this conditional.
         dialog_fadeout();
 
-        src_board->input_size = strlen(src_board->input_string);
-        src_board->num_input = atoi(src_board->input_string);
+        len = strlen(input_buffer);
+        board_set_input_string(src_board, input_buffer, len);
+        src_board->input_size = len;
+        src_board->num_input = len ? atoi(src_board->input_string) : 0;
         break;
       }
 
       case ROBOTIC_CMD_IF_INPUT: // If string "" "l"
       {
+        const char *input_string = src_board->input_string ? src_board->input_string : "";
         char cmp_buffer[ROBOT_MAX_TR];
         tr_msg(mzx_world, cmd_ptr + 2, id, cmp_buffer);
-        if(!strcasecmp(cmp_buffer, src_board->input_string))
+        if(!strcasecmp(cmp_buffer, input_string))
         {
           char *p2 = next_param_pos(cmd_ptr + 1);
           gotoed = send_self_label_tr(mzx_world, p2 + 1, id);
@@ -3920,9 +3925,10 @@ void run_robot(context *ctx, int id, int x, int y)
 
       case ROBOTIC_CMD_IF_INPUT_NOT: // If string not "" "l"
       {
+        const char *input_string = src_board->input_string ? src_board->input_string : "";
         char cmp_buffer[ROBOT_MAX_TR];
         tr_msg(mzx_world, cmd_ptr + 2, id, cmp_buffer);
-        if(strcasecmp(cmp_buffer, src_board->input_string))
+        if(strcasecmp(cmp_buffer, input_string))
         {
           char *p2 = next_param_pos(cmd_ptr + 1);
           gotoed = send_self_label_tr(mzx_world, p2 + 1, id);
@@ -3933,8 +3939,8 @@ void run_robot(context *ctx, int id, int x, int y)
       case ROBOTIC_CMD_IF_INPUT_MATCHES: // If string matches "" "l"
       {
         // compare
+        const char *input_string = src_board->input_string ? src_board->input_string : "";
         char cmp_buffer[ROBOT_MAX_TR];
-        char *input_string = src_board->input_string;
         size_t i, cmp_len;
         char current_char;
         char cmp_char;
@@ -5067,33 +5073,27 @@ void run_robot(context *ctx, int id, int x, int y)
 
       case ROBOTIC_CMD_CLIP_INPUT: // Clip input
       {
-        size_t i = 0, input_size = src_board->input_size;
-        char *input_string = src_board->input_string;
+        const char *input_string = src_board->input_string ? src_board->input_string : "";
+        size_t input_size = src_board->input_size;
+        size_t i = 0;
 
         // Chop up to and through first section of whitespace.
         // First, until non space or end
-        if(input_size)
+        while(*input_string && i < input_size)
         {
-          do
-          {
-            if(input_string[i] == 32)
-              break;
-          } while((++i) < input_size);
+          if(*input_string == ' ')
+            break;
+          input_string++;
+          i++;
         }
 
-        if(input_string[i] == 32)
-        {
-          do
-          {
-            if(input_string[i] != 32)
-              break;
-          } while((++i) < input_size);
-        }
-        // Chop UNTIL i. (i points to first No-Chop)
+        while(*input_string == ' ' && i < input_size)
+          input_string++, i++;
 
-        strcpy(input_string, input_string + i);
-        src_board->input_size = strlen(input_string);
-        src_board->num_input = atoi(input_string);
+        i = strlen(input_string);
+        board_set_input_string(src_board, input_string, i);
+        src_board->input_size = i;
+        src_board->num_input = i ? atoi(src_board->input_string) : 0;
         break;
       }
 
@@ -5640,9 +5640,13 @@ void run_robot(context *ctx, int id, int x, int y)
 
       case ROBOTIC_CMD_IF_FIRST_INPUT: // If first string str str
       {
+        char tmp[2] = "";
         char *input_string = src_board->input_string;
         char match_string_buffer[ROBOT_MAX_TR];
         ssize_t i = 0;
+
+        if(!input_string)
+          input_string = tmp;
 
         if(src_board->input_size)
         {
@@ -5650,7 +5654,7 @@ void run_robot(context *ctx, int id, int x, int y)
           {
             if(input_string[i] == 32) break;
             i++;
-          } while(i < (ssize_t)src_board->input_size);
+          } while(i < (ssize_t)src_board->input_size && input_string[i]);
         }
 
         if(input_string[i] == 32)

src/board.c
@@ -38,6 +38,7 @@
 static int save_board_info(struct board *cur_board, struct zip_archive *zp,
  int savegame, int file_version, const char *name)
 {
+  const char *tmp;
   char *buffer;
   struct memfile mf;
   int result;
@@ -89,16 +90,13 @@ static int save_board_info(struct board *cur_board, struct zip_archive *zp,
   save_prop_c(BPROP_PLAYER_ATTACK_LOCKED, cur_board->player_attack_locked, &mf);
   save_prop_c(BPROP_RESET_ON_ENTRY, cur_board->reset_on_entry, &mf);
 
-  if(cur_board->charset_path)
-  {
-    length = strlen(cur_board->charset_path);
-    save_prop_s(BPROP_CHARSET_PATH, cur_board->charset_path, length, 1, &mf);
-  }
-  if(cur_board->palette_path)
-  {
-    length = strlen(cur_board->palette_path);
-    save_prop_s(BPROP_PALETTE_PATH, cur_board->palette_path, length, 1, &mf);
-  }
+  tmp = cur_board->charset_path ? cur_board->charset_path : "";
+  length = strlen(tmp);
+  save_prop_s(BPROP_CHARSET_PATH, tmp, length, 1, &mf);
+
+  tmp = cur_board->palette_path ? cur_board->palette_path : "";
+  length = strlen(tmp);
+  save_prop_s(BPROP_PALETTE_PATH, tmp, length, 1, &mf);
 
   if(savegame)
   {
@@ -112,8 +110,9 @@ static int save_board_info(struct board *cur_board, struct zip_archive *zp,
     save_prop_d(BPROP_NUM_INPUT, cur_board->num_input, &mf);
     save_prop_d(BPROP_INPUT_SIZE, cur_board->input_size, &mf);
 
-    length = strlen(cur_board->input_string);
-    save_prop_s(BPROP_INPUT_STRING, cur_board->input_string, length, 1, &mf);
+    tmp = cur_board->input_string ? cur_board->input_string : "";
+    length = strlen(tmp);
+    save_prop_s(BPROP_INPUT_STRING, tmp, length, 1, &mf);
 
     length = strlen(cur_board->bottom_mesg);
     save_prop_s(BRPOP_BOTTOM_MESG, cur_board->bottom_mesg, length, 1, &mf);
@@ -296,7 +295,8 @@ static void default_board(struct board *cur_board)
   cur_board->last_key = '?';
   cur_board->num_input = 0;
   cur_board->input_size = 0;
-  cur_board->input_string[0] = 0;
+  cur_board->input_allocated = 0;
+  cur_board->input_string = NULL;
   cur_board->bottom_mesg[0] = 0;
   cur_board->b_mesg_timer = 0;
   cur_board->b_mesg_row = 24;
@@ -337,6 +337,26 @@ void dummy_board(struct board *cur_board)
   cur_board->num_sensors = 0;
   cur_board->num_sensors_allocated = 0;
   cur_board->sensor_list = ccalloc(1, sizeof(struct sensor *));
+}
+
+void board_set_input_string(struct board *cur_board, const char *input, size_t len)
+{
+  if(!len || !input || !input[0])
+  {
+    if(cur_board->input_string)
+      cur_board->input_string[0] = '\0';
+    return;
+  }
+
+  if(len + 1 > cur_board->input_allocated)
+  {
+    size_t size = MAX(len + 1, 81);
+    cur_board->input_string = crealloc(cur_board->input_string, size);
+    cur_board->input_allocated = size;
+  }
+  // The provided input may be a part of the input string!
+  memmove(cur_board->input_string, input, len);
+  cur_board->input_string[len] = '\0';
 }
 
 __editor_maybe_static void board_set_charset_path(struct board *cur_board,
@@ -628,8 +648,7 @@ static int load_board_info(struct board *cur_board, struct zip_archive *zp,
 
       case BPROP_INPUT_STRING:
         size = MIN(size, ROBOT_MAX_TR-1);
-        mfread(cur_board->input_string, size, 1, &prop);
-        cur_board->input_string[size] = 0;
+        board_set_input_string(cur_board, (const char *)prop.start, size);
         break;
 
       case BRPOP_BOTTOM_MESG:
@@ -1218,6 +1237,14 @@ struct board *duplicate_board(struct world *mzx_world,
   }
 
   // Buffers
+  if(src_board->input_string)
+  {
+    dest_board->input_string = NULL;
+    dest_board->input_allocated = 0;
+    board_set_input_string(dest_board, src_board->input_string,
+     src_board->input_allocated);
+  }
+
   if(src_board->charset_path)
   {
     dest_board->charset_path = NULL;
@@ -1330,6 +1357,7 @@ void clear_board(struct board *cur_board)
   free(cur_board->level_under_param);
   free(cur_board->level_under_color);
 
+  free(cur_board->input_string);
   free(cur_board->charset_path);
   free(cur_board->palette_path);
 

src/board.h
@@ -34,6 +34,8 @@ CORE_LIBSPEC int save_board(struct world *mzx_world, struct board *cur_board,
 CORE_LIBSPEC struct board *load_board_allocate(struct world *mzx_world,
  struct zip_archive *zp, int savegame, int file_version, unsigned int board_id);
 
+CORE_LIBSPEC void board_set_input_string(struct board *cur_board,
+ const char *input, size_t len);
 CORE_LIBSPEC void clear_board(struct board *cur_board);
 
 struct board *duplicate_board(struct world *mzx_world,

src/board_struct.h
@@ -66,7 +66,8 @@ struct board
   int last_key;
   int num_input;
   size_t input_size;
-  char input_string[ROBOT_MAX_TR];
+  size_t input_allocated;
+  char *input_string;
   int player_last_dir;
   char bottom_mesg[ROBOT_MAX_TR];
   int b_mesg_timer;

src/counter.c
@@ -35,6 +35,7 @@
 #include <sys/time.h>
 #endif /* _MSC_VER */
 
+#include "board.h"
 #include "configure.h"
 #include "counter.h"
 #include "data.h"
@@ -1161,8 +1162,10 @@ static int input_read(struct world *mzx_world,
 static void input_write(struct world *mzx_world,
  const struct function_counter *counter, const char *name, int value, int id)
 {
+  char buf[12];
+  sprintf(buf, "%d", value);
+  board_set_input_string(mzx_world->current_board, buf, strlen(buf));
   mzx_world->current_board->num_input = value;
-  sprintf(mzx_world->current_board->input_string, "%d", value);
 }
 
 static int key_read(struct world *mzx_world,

src/editor/board.c
@@ -263,7 +263,8 @@ struct board *create_blank_board(struct editor_config_info *conf)
   cur_board->last_key = '?';
   cur_board->num_input = 0;
   cur_board->input_size = 0;
-  cur_board->input_string[0] = 0;
+  cur_board->input_allocated = 0;
+  cur_board->input_string = NULL;
   cur_board->player_last_dir = 0x10;
   cur_board->bottom_mesg[0] = 0;
   cur_board->b_mesg_timer = 0;

src/editor/debug.c
@@ -572,9 +572,14 @@ static void update_ram_usage_data(struct world *mzx_world,
   for(i = 0; i < mzx_world->num_boards; i++)
   {
     struct board *b = mzx_world->board_list[i];
+    // Board data.
     ram_data.board_data_size += b->board_width * b->board_height * 6;
     if(b->overlay_mode)
       ram_data.board_overlay_size += b->board_width * b->board_height * 2;
+
+    // Buffers (split off the board struct).
+    ram_data.board_list_and_struct_size +=
+     b->input_allocated + b->charset_path_allocated + b->palette_path_allocated;
 
     // Robot list (+1) and robot name sorted list.
     ram_data.robot_list_and_struct_size +=
@@ -631,7 +636,7 @@ int get_counter_safe(struct world *mzx_world, const char *name, int id)
   return get_counter(mzx_world, name, id);
 }
 
-static void get_var_name(struct debug_var *v, char **name, int *len,
+static void get_var_name(struct debug_var *v, const char **name, int *len,
  char buffer[VAR_LIST_WIDTH + 1])
 {
   switch((enum debug_var_type)v->type)
@@ -681,7 +686,7 @@ static void get_var_name(struct debug_var *v, char **name, int *len,
 
 // The buffer param is used for any vars that need to generate char values.
 static void get_var_value(struct world *mzx_world, struct debug_var *v,
- char **char_value, int *int_value, int64_t *long_value,
+ const char **char_value, int *int_value, int64_t *long_value,
  char buffer[VAR_LIST_WIDTH + 1])
 {
   struct board *cur_board = mzx_world->current_board;
@@ -780,7 +785,7 @@ static void get_var_value(struct world *mzx_world, struct debug_var *v,
       if(match_var("input*"))
       {
         // the starred version of input is the input string!
-        *char_value = cur_board->input_string;
+        *char_value = cur_board->input_string ? cur_board->input_string : "";
         *int_value = strlen(*char_value);
       }
       else
@@ -1008,7 +1013,7 @@ static void read_var(struct world *mzx_world, struct debug_var *v)
 {
   char buffer[VAR_LIST_WIDTH + 1];
   char *char_dest = v->text + SVALUE_COL_OFFSET;
-  char *char_value = NULL;
+  const char *char_value = NULL;
   int64_t long_value = INT64_MIN;
   int int_value = 0;
 
@@ -1633,7 +1638,7 @@ static boolean search_vars(struct world *mzx_world, struct debug_node *node,
 
   char var_text_buffer[VAR_LIST_WIDTH + 1];
   int var_text_length;
-  char *var_text;
+  const char *var_text;
 
   if(search_flags & VAR_SEARCH_REVERSE)
   {
@@ -3300,7 +3305,7 @@ void __debug_counters(context *ctx)
   if(var_selected < num_vars)
   {
     char buffer[VAR_LIST_WIDTH + 1];
-    char *var_name;
+    const char *var_name;
     int len;
 
     get_var_name(var_list[var_selected], &var_name, &len, buffer);

src/expr.c
@@ -543,14 +543,15 @@ int parse_expression(struct world *mzx_world, char **_expression, int *error,
     // Interpolate a counter or string
     if(state & EXPR_STATE_INTERPOLATING)
     {
+      const char *src;
       size_t len;
-      char *src;
       buf_pos = buffer + buf_start;
 
       // Input
       if(!memcasecmp(buf_pos, "INPUT", 6))
       {
         src = mzx_world->current_board->input_string;
+        src = src ? src : "";
         len = strlen(src);
       }
       else

commit 30adc90d373f647429d82c4db32b0b96c5cba31f

Fix INPUT STRING title regression caused by 57e798e6.

src/run_robot.c
@@ -3897,7 +3897,7 @@ void run_robot(context *ctx, int id, int x, int y)
         input_buffer[0] = 0;
 
         dialog_fadein();
-        input_window(mzx_world, input_buffer, input_buffer, 70);
+        input_window(mzx_world, title_buffer, input_buffer, 70);
 
         // Due to a faulty check, 2.83 through 2.91f always stay faded in here.
         // If something is found that relies on that, make this conditional.