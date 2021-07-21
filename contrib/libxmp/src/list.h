4df6e4845b95a19fb450e89a8c360390c59bd5c6
AliceLR <petrifiedrowan@gmail.com>
2021-07-20 20:46:49 -0600

    Temporary patch for libxmp list.h undefined behavior hack.

--- contrib/libxmp/src/list.h
+++ contrib/libxmp/src/list.h
@@ -133,7 +133,7 @@ static __inline__ void list_splice(struct list_head *list, struct list_head *hea
  * @member:	the name of the list_struct within the struct.
  */
 #define list_entry(ptr, type, member) \
-	((type *)((char *)(ptr)-(size_t)(&((type *)0)->member)))
+	((type *)((char *)(ptr) - offsetof(type, member)))
 
 /**
  * list_for_each	-	iterate over a list
