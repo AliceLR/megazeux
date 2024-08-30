dd875332682ad97ae1db9ee1080cb6702c56b6c2
AliceLR <petrifiedrowan@gmail.com>
2024-08-30 16:48:01 -0600

    Fix bundling of macOS PowerPC64 dependencies.

--- arch/darwin/bundle.sh
+++ arch/darwin/bundle.sh
@@ -3,6 +3,13 @@
 ARCH=$1
 shift
 
+# PPC64 hotfix--replace otool with otool-classic.
+# LLVM-based otool uses llvm-objdump, does not support PowerPC64 LC_UNIXTHREAD.
+# dylibbundler currently does not support replacing otool by other means.
+if [ "$ARCH" = "ppc64" ]; then
+	export PATH="$(pwd)/arch/darwin/bin:$PATH"
+fi
+
 for exe in $@; do
 	if [ -f "$exe" ]; then
 		mkdir -p ./bundles
