bf6f8ad1026cfd2bf74da37c35bb14b95a049759
Alice R. <petrifiedrowan@gmail.com>
2024-09-03 02:14:15 -0600

    Fix darwin-dist Gatekeeper errors caused by bad codesign handling. (#471)
    * Fix darwin-dist codesigning, move libraries to Frameworks dir, changelog.
    * Update scripts/deps/ SDL2 latest to 2.30.6.

--- arch/darwin/bundle.sh
+++ arch/darwin/bundle.sh
@@ -10,10 +10,15 @@ if [ "$ARCH" = "ppc64" ]; then
 	export PATH="$(pwd)/arch/darwin/bin:$PATH"
 fi
 
+# Note: do not codesign here since there's no guarantee the MZX binaries
+# are being processed by dylibbundler in the "correct" order for codesign.
+# Signing should instead be performed during the .app bundling stage,
+# after the executables have been combined with lipo.
+#
 for exe in $@; do
 	if [ -f "$exe" ]; then
 		mkdir -p ./bundles
-		dylibbundler -cd -of -b -x $exe \
-		  -d "./bundles/libs-$ARCH/" -p "@executable_path/../libs-$ARCH/"
+		dylibbundler -ns -cd -of -b -x $exe \
+		  -d "./bundles/libs-$ARCH/" -p "@executable_path/../Frameworks/libs-$ARCH/"
 	fi
 done
