b8e05d1d9b8383f3ce0585a04a03743f9c5d3358
Adrian Siekierka <kontakt@asie.pl>
2024-12-01 10:30:46 +0100

    android: Support 16K page sizes. (#506)
    As of Android 15, devices will be able to ship with 16 KB page sizes instead
    of the previous default of 4 KB. This requires updating the NDK (which we
    are not doing as to keep compatibility with API level 16 for 2.93c) or
    adjusting the build process of all shared libraries (which is done by
    this commit).

--- arch/android/libogg-Android.mk
+++ arch/android/libogg-Android.mk
@@ -6,6 +6,9 @@ LOCAL_MODULE := libogg
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
 
+# https://developer.android.com/guide/practices/page-sizes
+LOCAL_LDFLAGS += "-Wl,-z,max-page-size=16384"
+
 LOCAL_SRC_FILES := src/bitwise.c src/framing.c
 
 include $(BUILD_SHARED_LIBRARY)
