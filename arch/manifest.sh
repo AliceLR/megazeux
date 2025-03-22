e16a8e0e91dfaf1bce00890584f203cd52f6df26
AliceLR <petrifiedrowan@gmail.com>
2025-03-22 04:20:27 -0600

    Remove bashisms and other issues from manifest.sh.
    * Removed Bash-style function declaration. (Bash extension)
    * Removed function export. (Bash extension)
    * Explicitly provide . to find. (ShellCheck)
    * Use ! instead of -not for find. (some BusyBox builds apparently)
    * Rewrite xargs invocation to use -n1 instead of -I{}. (xargs warning)
    * The manifest entry script is now provided directly to xargs
      instead of in a function, which looks worse, but is
      POSIX-compatible.
    * Replace legacy backtick usage with $(). (ShellCheck)
    * Double quote all variable expansions. (ShellCheck)
    * Double quote space separator argument to cut. (ShellCheck)
    * Use set -e for cd. (ShellCheck)
    * Use sort -k3 instead of --key=3. (BusyBox)

--- arch/manifest.sh
+++ arch/manifest.sh
@@ -1,32 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 
+set -e
 if [ "$1" = "" ]; then
 	echo "usage: $0 [build dir]"
 	exit 0
 fi
 
-function getManifestEntry
-{
-	size=`du -b $1 | cut -f1`
-	sha256sum=`sha256sum -b $1 | cut -f1 -d\ `
-	echo $sha256sum $size `echo $1 | sed 's,\.\/,,'`
-}
-
-# can't execute the above function via xargs unless it is exported
-export -f getManifestEntry
-
 echo "Generating manifest..."
 
-cd $1
+cd "$1"
 
 rm -f manifest.txt
 
-find -mindepth 1 -type f				\
-	-not -name config.txt				\
-	-not -name manifest.txt				\
-	-not -name pad.config				\
-	-not -name '*.debug' 				\
+# -P24 is a GNU-ism but also the entire point of using xargs here.
+# Most BSD xargs seem to support it; BusyBox gracefully ignores it.
+find . -mindepth 1 -type f				\
+	! -name config.txt				\
+	! -name manifest.txt				\
+	! -name pad.config				\
+	! -name '*.debug' 				\
 	-print0						\
-| xargs -0 -n1 -I{} -P24 bash -c 'getManifestEntry {}'	\
-| sort --key=3						\
+| xargs -0 -n1 -P24 /bin/sh -c				\
+	'size=$(du -b "$1" | cut -f1);			\
+	 sha=$(sha256sum -b "$1" | cut -f1 -d" ");	\
+	 name=$(echo "$1" | sed "s,\\.\\/,,");		\
+	 echo "$sha" "$size" "$name"' "$0"		\
+| sort -k3						\
 >> manifest.txt
