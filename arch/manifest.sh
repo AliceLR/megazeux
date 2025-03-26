baf04d3866152df205ae038f1fd0581f023e9ac2
Alice R. <petrifiedrowan@gmail.com>
2025-03-26 03:29:31 -0600

    Remove bashisms and other issues from manifest.sh. (#529)
    * Removed Bash-style function declaration. (Bash extension)
    * Removed function export. (Bash extension)
    * Explicitly provide . to find. (ShellCheck)
    * Use ! instead of -not for find. (some BusyBox builds apparently)
    * Rewrite xargs invocation to use -n1 instead of -I{}. (xargs warning)
    * The manifest entry script is now provided directly to xargs
      instead of in a function, which looks worse, but is
      POSIX-compatible.
    * Replace legacy backtick usage with $(). (ShellCheck)
    * Double quote all variable expansions. (ShellCheck)
    * Double quote space separator argument to cut. (ShellCheck)
    * Use set -e for cd. (ShellCheck)
    * Use sort -k3 instead of --key=3. (BusyBox)
    * Support gsha256sum, BSD sha256, Perl shasum. (macOS, NetBSD, FreeBSD, OpenBSD)
    * Use wc -c instead of du -b. (macOS, NetBSD, FreeBSD, OpenBSD)

--- arch/manifest.sh
+++ arch/manifest.sh
@@ -1,32 +1,42 @@
-#!/bin/bash
+#!/bin/sh
 
+set -e
 if [ "$1" = "" ]; then
 	echo "usage: $0 [build dir]"
 	exit 0
 fi
 
-function getManifestEntry
-{
-	size=`du -b $1 | cut -f1`
-	sha256sum=`sha256sum -b $1 | cut -f1 -d\ `
-	echo $sha256sum $size `echo $1 | sed 's,\.\/,,'`
-}
-
-# can't execute the above function via xargs unless it is exported
-export -f getManifestEntry
+if command -v sha256sum >/dev/null 2>/dev/null; then
+	export sha256cmd="sha256sum -b"
+elif command -v gsha256sum >/dev/null 2>/dev/null; then
+	export sha256cmd="gsha256sum -b"
+elif command -v sha256 >/dev/null 2>/dev/null; then
+	export sha256cmd="sha256 -q"
+elif command -v shasum >/dev/null 2>/dev/null; then
+	export sha256cmd="shasum -a256 -b"
+else
+	echo "error: install sha256sum (coreutils, BusyBox) or shasum (perl)."
+	exit 1
+fi
 
 echo "Generating manifest..."
 
-cd $1
+cd "$1"
 
 rm -f manifest.txt
 
-find -mindepth 1 -type f				\
-	-not -name config.txt				\
-	-not -name manifest.txt				\
-	-not -name pad.config				\
-	-not -name '*.debug' 				\
+# -P24 is a GNU-ism but also the entire point of using xargs here.
+# Most BSD xargs seem to support it; BusyBox gracefully ignores it.
+find . -mindepth 1 -type f				\
+	! -name config.txt				\
+	! -name manifest.txt				\
+	! -name pad.config				\
+	! -name '*.debug' 				\
 	-print0						\
-| xargs -0 -n1 -I{} -P24 bash -c 'getManifestEntry {}'	\
-| sort --key=3						\
+| xargs -0 -n1 -P24 /bin/sh -c				\
+	'size=$(($(wc -c <"$1")));			\
+	 sha=$($sha256cmd "$1" | cut -f1 -d" ");	\
+	 name=$(echo "$1" | sed "s,\\.\\/,,");		\
+	 echo "$sha" "$size" "$name"' "$0"		\
+| sort -k3						\
 >> manifest.txt
